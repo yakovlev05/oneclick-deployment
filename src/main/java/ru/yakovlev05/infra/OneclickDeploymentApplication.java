9362ef257163ae61cb981cf60b6aa8a5bacad274
yakovlev05 <alexey.yakovlev.05@mail.ru>
2026-07-19 01:07:13 +0500

    init project

---
+++ src/main/java/ru/yakovlev05/infra/OneclickDeploymentApplication.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneclickDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneclickDeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yakovlev05/infra/OneclickDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.infra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneclickDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
